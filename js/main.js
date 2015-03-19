15f88bfe5ecb860d875a129b27959f87ad8ce10c
shelbyboyd <shelbyboyd@Shelbys-MacBook-Pro-2.local>
2015-03-19 11:07:31 -0400

    added buttons

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ var $navBtn = $ ('.nav-button');
 $navBtn.on ('click', function () {
     $nav.toggleClass ('nav-show');
 });
+
+var $mobile = $('.mobile');
+var $mobileBtn = $('.mobile-button');
+                   
+$mobileBtn.on ('click', function () {
+    $mobile.toggleClass ('mobile-show');
+});
+    
+var $email = $('.email');
+var $emailBtn = $('.email-button');
+                   
+$emailBtn.on ('click', function () {
+    $email.toggleClass ('email-show');
+});
+    
+var $behance = $('.behance');
+var $behanceBtn = $('.behance-button');
+                   
+$behanceBtn.on ('click', function () {
+    $behance.toggleClass ('behance-show');
+});
+    
+
+    
